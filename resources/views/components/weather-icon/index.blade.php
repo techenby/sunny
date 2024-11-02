69aecfc9419fa3b1df84bc98219bbd693f318776
Andy Newhouse <hi@andymnewhouse.me>
2024-11-02 15:26:00 -0500

    Make sun default

--- resources/views/components/weather-icon/index.blade.php
+++ resources/views/components/weather-icon/index.blade.php
@@ -7,10 +7,10 @@ $component = match(true) {
     $id >= 500 && $id <= 599 => 'weather-icon.sun-behind-rain-cloud',
     $id >= 600 && $id <= 699 => 'weather-icon.snow-flake',
     $id >= 700 && $id <= 799 => 'weather-icon.fog',
-    $id === 800 => 'weather-icon.sun',
     $id === 801 => 'weather-icon.sun-behind-small-cloud',
     $id === 802 => 'weather-icon.sun-behind-large-cloud',
     $id === 803 || $id === 804 => 'weather-icon.cloud',
+    default => 'weather-icon.sun',
 }
 @endphp
 
