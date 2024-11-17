e78771a1a0698922aed6b45dd3b7260e9f38fbe1
Andy Newhouse <hi@andymnewhouse.me>
2024-11-17 13:12:55 -0600

    Add ability to add/edit tiles

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Livewire\Pages\Inventory\Bins;
 use App\Livewire\Pages\Inventory\Locations;
 use App\Livewire\Pages\Inventory\Things;
+use App\Livewire\Pages\LogPose\Tiles;
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
@@ -23,6 +24,8 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('inventory/locations', Locations::class)->name('inventory.locations');
     Route::get('inventory/bins', Bins::class)->name('inventory.bins');
     Route::get('inventory/things', Things::class)->name('inventory.things');
+
+    Route::get('log-pose/tiles', Tiles::class)->name('log-pose.tiles');
 
     Volt::route('/users', 'pages.users')->name('users');
 });
---
+++ tests/Feature/Livewire/LogPose/TilesTest.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Livewire\Pages\LogPose\Tiles;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Sequence;
+use Livewire\Livewire;
+use Spatie\Dashboard\Models\Tile;
+
+test('can view page', function () {
+    $user = User::factory()->create();
+
+    $this->actingAs($user)
+        ->get('/log-pose/tiles')
+        ->assertOk()
+        ->assertSee('Tiles');
+});
+
+test('can view component', function () {
+    Tile::create(['name' => 'calendar-andy']);
+    Tile::create(['name' => 'calendar-family']);
+
+    Livewire::test(Tiles::class)
+        ->assertSee('Tiles')
+        ->assertSee(['calendar-andy', 'calendar-family']);
+});
+
+test('can create tile', function () {
+    Livewire::test(Tiles::class)
+        ->assertSee('Create')
+        ->set('name', 'coworkers-andy')
+        ->set('data', "[['name'=>'Andy','location'=>'Chicago, IL','timezone'=>'America/Chicago']]")
+        ->call('save')
+        ->assertSet('name', '')
+        ->assertSet('data', '[]');
+
+    $this->assertDatabaseHas('dashboard_tiles', [
+        'name' => 'coworkers-andy',
+    ]);
+});
