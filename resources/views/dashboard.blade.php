9502c75d8218616f977bc9df4456796173856c17
Andy Newhouse <hi@andymnewhouse.me>
2024-11-23 19:42:25 -0600

    Add calendar to homepage

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,34 @@
 <x-app-layout>
+    @push('head')
+    <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.15/index.global.min.js'></script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            var calendarEl = document.getElementById('calendar');
+            var calendar = new FullCalendar.Calendar(calendarEl, {
+                initialView: 'timeGridWeek',
+                nowIndicator: true,
+                scrollTime: '8:00:00',
+                headerToolbar: {
+                    left: 'prev,next',
+                    center: 'title',
+                    right: 'payPeriod,timeGridWeek,dayGridMonth'
+                },
+                views: {
+                    payPeriod: {
+                        buttonText: 'pay period',
+                        type: 'dayGrid',
+                        visibleRange: {
+                            start: '2024-11-22',
+                            end: '2024-12-06'
+                        }
+                    }
+                },
+            });
+            calendar.render();
+        });
+    </script>
+    @endpush
     <flux:main class="space-y-6">
-        <flux:card>
-            <flux:heading>{{ __("You're logged in!") }}</flux:heading>
-        </flux:card>
+        <div id="calendar"></div>
     </flux:main>
 </x-app-layout>
