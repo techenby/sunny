329d733cc33e57d9d331acc6d031f078e70f4eba
Andy Newhouse <hi@andymnewhouse.me>
2024-12-07 10:58:12 -0600

    Add status list to user

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,7 @@ class User extends Authenticatable implements MustVerifyEmail
         'email',
         'password',
         'status',
+        'status_list'
     ];
 
     /**
@@ -40,6 +41,7 @@ class User extends Authenticatable implements MustVerifyEmail
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'status_list' => 'array',
         ];
     }
 
