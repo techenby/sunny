fd81ccd24927e98e9b45093f4e628a66ee8b761a
Andy Newhouse <hi@andymnewhouse.me>
2024-12-07 17:37:52 -0600

    Fix profile page

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,27 +1,23 @@
 <x-app-layout>
-    <x-slot name="header">
+    <flux:main class="space-y-6">
         <flux:heading size="xl" level="2">{{ __('Profile') }}</flux:heading>
-    </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <flux:card>
-                <div class="max-w-xl">
-                    <livewire:profile.update-profile-information-form />
-                </div>
-            </flux:card>
+        <flux:card>
+            <div class="max-w-xl">
+                <livewire:profile.update-profile-information-form />
+            </div>
+        </flux:card>
 
-            <flux:card>
-                <div class="max-w-xl">
-                    <livewire:profile.update-password-form />
-                </div>
-            </flux:card>
+        <flux:card>
+            <div class="max-w-xl">
+                <livewire:profile.update-password-form />
+            </div>
+        </flux:card>
 
-            <flux:card>
-                <div class="max-w-xl">
-                    <livewire:profile.delete-user-form />
-                </div>
-            </flux:card>
-        </div>
-    </div>
+        <flux:card>
+            <div class="max-w-xl">
+                <livewire:profile.delete-user-form />
+            </div>
+        </flux:card>
+    </flux:main>
 </x-app-layout>
