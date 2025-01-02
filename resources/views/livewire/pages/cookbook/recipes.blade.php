20d97593198f6fa9219d0eeec6e8f108e0b9d311
Andy Newhouse <hi@andymnewhouse.me>
2025-01-01 20:00:39 -0600

    Make mobile recipes better

--- resources/views/livewire/pages/cookbook/recipes.blade.php
+++ resources/views/livewire/pages/cookbook/recipes.blade.php
@@ -65,24 +65,5 @@
         </flux:table>
     </section>
 
-    @guest
-    <footer class="absolute bottom-0 left-0 w-full [grid-area:main] p-6 lg:p-8 [[data-flux-container]_&]:px-0  space-y-6">
-        <flux:separator />
-        <flux:subheading class="align-baseline text-center">
-            Built with <flux:icon.heart class="inline" variant="mini" /> by
-            <flux:link variant="ghost" href="https://techenby.com">
-                Andy Newhouse
-            </flux:link>
-            using <flux:link variant="ghost" href="https://laravel.com/">Laravel</flux:link>,
-            <flux:link variant="ghost" href="https://livewire.laravel.com/">Livewire</flux:link>,
-            <flux:link variant="ghost" href="https://fluxui.dev/">Flux</flux:link>.
-        </flux:subheading>
-        <flux:subheading class="align-baseline text-center">
-            Hosted on <flux:link variant="ghost" href="https://digitalocean.com">Digital Ocean</flux:link> via
-            <flux:link variant="ghost" href="https://forge.laravel.com">Laravel Forge</flux:link>.
-            View the sourcecode on
-            <flux:link variant="ghost" href="https://github.com/techenby/sunny">GitHub</flux:link>.
-        </flux:subheading>
-    </footer>
-    @endguest
+    @includeWhen(auth()->guest(), 'layouts.guest-footer')
 </flux:main>
--- resources/views/livewire/pages/cookbook/show-recipe.blade.php
+++ resources/views/livewire/pages/cookbook/show-recipe.blade.php
@@ -1,70 +1,71 @@
-<flux:main>
-    <div class="flex h-full">
-        <div class="w-2/3 space-y-6 pr-4">
-            <header class="flex">
-                <flux:heading size="xl" level="1">{{ $recipe->name }}</flux:heading>
-                <flux:spacer />
-                @auth
-                <flux:button :href="route('cookbook.recipes.edit', $recipe)" size="sm" icon="pencil"
-                    icon-variant="outline" square>
-                <span class="sr-only">Edit</span></flux:button>
-                @endauth
-            </header>
+<flux:main class="space-y-6">
+    <header class="lg:border-b lg:mb-8 lg:pb-4 space-y-6">
+        <div class="flex">
+            <flux:heading size="xl" level="1">{{ $recipe->name }}</flux:heading>
+            <flux:spacer />
+            @auth
+            <flux:button :href="route('cookbook.recipes.edit', $recipe)" size="sm" icon="pencil"
+                icon-variant="outline" square>
+            <span class="sr-only">Edit</span></flux:button>
+            @endauth
+        </div>
 
-            <dl class="text-sm flex divide-x text-zinc-900 dark:text-zinc-200 divide-zinc-800/15 dark:divide-white/20">
-                @if ($recipe->source)
-                <div class="flex space-x-1 items-center pr-2">
-                    <dt><flux:icon.bookmark-square class="size-5" /></dt>
-                    <dd>
-                        @if (str_contains($recipe->source, 'http'))
-                        <flux:link variant="ghost" :href="$recipe->source">
-                            {{ $recipe->shortened_source }}
-                        </flux:link>
-                        @else
+        <dl class="text-sm flex divide-x text-zinc-900 dark:text-zinc-200 divide-zinc-800/15 dark:divide-white/20">
+            @if ($recipe->source)
+            <div class="flex space-x-1 items-center pr-2">
+                <dt><flux:icon.bookmark-square class="size-5" /></dt>
+                <dd>
+                    @if (str_contains($recipe->source, 'http'))
+                    <flux:link variant="ghost" :href="$recipe->source">
                         {{ $recipe->shortened_source }}
-                        @endif
-                    </dd>
-                </div>
-                @endif
-                @if ($recipe->servings)
-                <div class="flex space-x-1 items-center px-2">
-                    <dt><flux:icon.users class="size-5" /></dt>
-                    <dd>{{ $recipe->servings }}</dd>
-                </div>
-                @endif
-                @if ($recipe->categories)
-                <div class="flex space-x-1 items-center px-2">
-                    <dt><flux:icon.tag class="size-5" /></dt>
-                    <dd>{{ $recipe->categories }}</dd>
-                </div>
-                @endif
-                @if ($recipe->prep_time)
-                <div class="flex flex-col space-y-1 items-center px-2">
-                    <dt class="text-[10px] text-zinc-700 dark:text-zinc-400 uppercase font-light">Prep</dt>
-                    <dd class="order-first">{{ $recipe->prep_time }}</dd>
-                </div>
-                @endif
-                @if ($recipe->cook_time)
-                <div class="flex flex-col space-y-1 items-center px-2">
-                    <dt class="text-[10px] text-zinc-700 dark:text-zinc-400 uppercase font-light">Cook</dt>
-                    <dd class="order-first">{{ $recipe->cook_time }}</dd>
-                </div>
-                @endif
-                @if ($recipe->total_time)
-                <div class="flex flex-col space-y-1 items-center pl-2">
-                    <dt class="text-[10px] text-zinc-700 dark:text-zinc-400 uppercase font-light">Total</dt>
-                    <dd class="order-first">{{ $recipe->total_time }}</dd>
-                </div>
-                @endif
-            </dl>
-
-            @if ($recipe->description)
-                <p class="text-sm text-zinc-900 dark:text-zinc-400">
-                    {{ $recipe->description }}
-                </p>
-                <flux:separator />
+                    </flux:link>
+                    @else
+                    {{ $recipe->shortened_source }}
+                    @endif
+                </dd>
+            </div>
             @endif
+            @if ($recipe->servings)
+            <div class="flex space-x-1 items-center px-2">
+                <dt><flux:icon.users class="size-5" /></dt>
+                <dd>{{ $recipe->servings }}</dd>
+            </div>
+            @endif
+            @if ($recipe->categories)
+            <div class="flex space-x-1 items-center px-2">
+                <dt><flux:icon.tag class="size-5" /></dt>
+                <dd>{{ $recipe->categories }}</dd>
+            </div>
+            @endif
+            @if ($recipe->prep_time)
+            <div class="flex flex-col space-y-1 items-center px-2">
+                <dt class="text-[10px] text-zinc-700 dark:text-zinc-400 uppercase font-light">Prep</dt>
+                <dd class="order-first">{{ $recipe->prep_time }}</dd>
+            </div>
+            @endif
+            @if ($recipe->cook_time)
+            <div class="flex flex-col space-y-1 items-center px-2">
+                <dt class="text-[10px] text-zinc-700 dark:text-zinc-400 uppercase font-light">Cook</dt>
+                <dd class="order-first">{{ $recipe->cook_time }}</dd>
+            </div>
+            @endif
+            @if ($recipe->total_time)
+            <div class="flex flex-col space-y-1 items-center pl-2">
+                <dt class="text-[10px] text-zinc-700 dark:text-zinc-400 uppercase font-light">Total</dt>
+                <dd class="order-first">{{ $recipe->total_time }}</dd>
+            </div>
+            @endif
+        </dl>
 
+        @if ($recipe->description)
+            <p class="text-sm text-zinc-900 dark:text-zinc-400">
+                {{ $recipe->description }}
+            </p>
+        @endif
+    </header>
+
+    <div class="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:h-full">
+        <div class="lg:col-span-2 space-y-6">
             <div class="prose prose-zinc dark:prose-invert">
                 {!! $recipe->instructions !!}
             </div>
@@ -76,10 +77,10 @@
                 </div>
             @endif
         </div>
-        <div class="w-1/3 space-y-6">
+        <div class="order-first lg:order-last space-y-6">
             @if ($url = $recipe->getFirstMediaUrl('thumb'))
-            <div class="relative bg-blue-100 dark:bg-blue-800">
-                <img src="{{ $url }}" alt="" class="object-cover h-48 w-96 rounded-md">
+            <div class="relative bg-blue-100 dark:bg-blue-800 rounded-xl overflow-hidden">
+                <img src="{{ $url }}" alt="" class="object-cover h-48 w-96">
             </div>
             @endif
             <flux:card>
@@ -89,4 +90,6 @@
             </flux:card>
         </div>
     </div>
+
+    @includeWhen(auth()->guest(), 'layouts.guest-footer')
 </flux:main>
