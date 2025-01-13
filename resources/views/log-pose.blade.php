539072e5cb087d1ff9d64f6eabb3738aa3990c09
Andy Newhouse <hi@andymnewhouse.me>
2025-01-13 09:15:20 -0600

    Update email

--- resources/views/log-pose.blade.php
+++ resources/views/log-pose.blade.php
@@ -1,6 +1,6 @@
 <x-dashboard>
     <livewire:tiles.clock position="a1:b1" />
-    <livewire:tiles.status position="a2" email="andymnewhouse@proton.me" />
+    <livewire:tiles.status position="a2" email="andymnewhouse@pm.me" />
     <livewire:tiles.status position="b2" email="ashnewhouse24@proton.me" />
     <livewire:tiles.monthly-calendar position="a3:a4" />
     <livewire:tiles.weather position="c1:c2" name="plainfield" />
