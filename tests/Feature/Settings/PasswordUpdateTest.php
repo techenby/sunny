a93fdd2f010c8774f90a43039896a1e8a42324fb
Andy Newhouse <andy@techenby.com>
2026-02-24 17:58:14 -0600

    Fix tests

--- tests/Feature/Settings/PasswordUpdateTest.php
+++ tests/Feature/Settings/PasswordUpdateTest.php
@@ -5,35 +5,31 @@ use Illuminate\Support\Facades\Hash;
 use Livewire\Livewire;
 
 test('password can be updated', function () {
-    $user = User::factory()->create([
+    $user = User::factory()->withTeam()->create([
         'password' => Hash::make('password'),
     ]);
 
-    $this->actingAs($user);
-
-    $response = Livewire::test('pages::settings.password')
+    Livewire::actingAs($user)
+        ->test('pages::settings.password')
         ->set('current_password', 'password')
         ->set('password', 'new-password')
         ->set('password_confirmation', 'new-password')
-        ->call('updatePassword');
-
-    $response->assertHasNoErrors();
+        ->call('updatePassword')
+        ->assertHasNoErrors();
 
     expect(Hash::check('new-password', $user->refresh()->password))->toBeTrue();
 });
 
 test('correct password must be provided to update password', function () {
-    $user = User::factory()->create([
+    $user = User::factory()->withTeam()->create([
         'password' => Hash::make('password'),
     ]);
 
-    $this->actingAs($user);
-
-    $response = Livewire::test('pages::settings.password')
+    Livewire::actingAs($user)
+        ->test('pages::settings.password')
         ->set('current_password', 'wrong-password')
         ->set('password', 'new-password')
         ->set('password_confirmation', 'new-password')
-        ->call('updatePassword');
-
-    $response->assertHasErrors(['current_password']);
+        ->call('updatePassword')
+        ->assertHasErrors(['current_password']);
 });
