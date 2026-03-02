8d45366a3fd1bc50c8a24c61d184c22dad0cc114
Andy Newhouse <andy@techenby.com>
2026-03-02 15:06:56 -0600

    Add team for default seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
+        User::factory()->withTeam()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
