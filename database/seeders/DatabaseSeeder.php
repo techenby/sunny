6842aaf257f8ae8756616d2401b58e82c81c0b81
Andy Newhouse <andy@techenby.com>
2026-03-03 15:13:02 -0600

    Add landing page

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,14 +10,9 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
+        User::factory()->withTeam()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
