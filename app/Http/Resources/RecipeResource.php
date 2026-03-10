bcbba8bf52fa39ec135bb068246643eb0e420e26
Andy Newhouse <andy@techenby.com>
2026-03-09 23:04:26 -0500

    Replace photo_path with photo_url for API

--- app/Http/Resources/RecipeResource.php
+++ app/Http/Resources/RecipeResource.php
@@ -6,12 +6,17 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Storage;
 
 class RecipeResource extends JsonResource
 {
     /** @return array<string, mixed> */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            ...Arr::except(parent::toArray($request), ['photo_path']),
+            'photo_url' => $this->photo_path ? Storage::url($this->photo_path) : null,
+        ];
     }
 }
