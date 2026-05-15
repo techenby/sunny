1418ff50592dbc94246a3b49d81e1fcb00be9a8f
Andy Swick <andy@techenby.com>
2026-05-15 14:02:04 -0500

    Remove calendar component from dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,3 +1,6 @@
 <x-layouts::app :title="__('Dashboard')">
-    <livewire:dashboard.calendar />
+    <flux:heading level="1" size="xl" class="mb-4">What would you like to see here?</flux:heading>
+    <flux:button icon="light-bulb" icon:trailing="arrow-up-right" href="https://suggest.gg/techenby" target="_blank">
+        {{ __('Make a Suggestion') }}
+    </flux:button>
 </x-layouts::app>
