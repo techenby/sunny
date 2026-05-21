a37fd9f39bbcc398480e7fde8afe0e6347e87811
Andy Swick <andy@techenby.com>
2026-05-21 13:18:47 -0500

    Move migration changes

---
+++ database/migrations/2026_05_21_000001_update_cache_expiration_columns_for_laravel_13.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('cache', function (Blueprint $table) {
+            $table->bigInteger('expiration')->change();
+        });
+
+        Schema::table('cache_locks', function (Blueprint $table) {
+            $table->bigInteger('expiration')->change();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('cache', function (Blueprint $table) {
+            $table->integer('expiration')->change();
+        });
+
+        Schema::table('cache_locks', function (Blueprint $table) {
+            $table->integer('expiration')->change();
+        });
+    }
+};
