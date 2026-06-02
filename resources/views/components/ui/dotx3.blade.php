754fb3ec86f04bf2f2380e7574456570049180d2
Andy Swick <andy@techenby.com>
2026-06-02 09:39:24 -0500

    Add dot dot dot loading component

---
+++ resources/views/components/ui/dotx3.blade.php
@@ -0,0 +1,2 @@
+<span class="text-2xl"><span class="animate-pulse">.</span><span class="animate-delay-600 animate-pulse">.</span><span class="animate-delay-1200 animate-pulse">.</span></span>
+{{-- Requires adding utility classes to app.css. @see https://www.hyperui.dev/blog/animation-duration-delay-with-tailwindcss/  --}}
