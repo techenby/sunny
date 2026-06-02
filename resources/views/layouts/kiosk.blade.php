4c039654d91f0071a565d71cd0ce6456bd360d06
Andy Swick <andy@techenby.com>
2026-06-02 17:27:58 -0500

    Add manual refresh button

--- resources/views/layouts/kiosk.blade.php
+++ resources/views/layouts/kiosk.blade.php
@@ -11,6 +11,17 @@
             <x-kiosk.sidebar.item icon="clipboard-document-list" :href="route('kiosk.chore-chart')" :current="request()->routeIs('kiosk.chore-chart')" wire:navigate>{{ __('Chores') }}</x-kiosk.sidebar.item>
             <x-kiosk.sidebar.item icon="queue-list" :href="route('kiosk.lists')" :current="request()->routeIs('kiosk.lists')" wire:navigate>{{ __('Lists') }}</x-kiosk.sidebar.item>
             <x-kiosk.sidebar.item icon="cooking-pot" :href="route('kiosk.meal-planning')" :current="request()->routeIs('kiosk.meal-planning')" wire:navigate>{{ __('Meals') }}</x-kiosk.sidebar.item>
+
+            <button
+                type="button"
+                class="mt-auto flex h-20 flex-col items-center justify-center p-2 text-zinc-700 hover:text-(--color-accent-content) dark:text-zinc-300"
+                data-kiosk-refresh
+                x-data
+                x-on:click="window.location.reload()"
+            >
+                <flux:icon icon="arrow-path" />
+                <flux:text>{{ __('Refresh') }}</flux:text>
+            </button>
         </x-kiosk.sidebar>
 
         <flux:main class="!p-0">
